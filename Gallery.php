d489a253a001568ed84f2f90f7ae262261305633
KhalidHossain3108 <86224418+KhalidHossain3108@users.noreply.github.com>
2021-06-30 22:39:33 +0600

    Update Gallery.php

--- Gallery.php
+++ Gallery.php
@@ -117,9 +117,9 @@ text-shadow:2px 2px 2px grey;
 
 
 <br><br><br><br>
-<img src="" width="100%" height="1000px">
+<img src="inbound804382635923766613.jpg" width="100%" height="1000px">
 <br><br>
-<font class="h2"> </font>
+<font class="h2"> Directing someone special how to click😆 </font>
 <br><br><br><br><br><br>
 <hr size="+12" color=#861D32><br><br>
 <hr size="+12" color=#861D32><br><br>
