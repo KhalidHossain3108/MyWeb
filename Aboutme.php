95c4e94028e868c2d91cb376423b6e45fb108e04
KhalidHossain3108 <86224418+KhalidHossain3108@users.noreply.github.com>
2021-06-30 20:15:11 +0600

    Update and rename inbound6962490937029481354.html to Aboutme.php

---
+++ Aboutme.php
@@ -0,0 +1,75 @@
+<html>
+<head>
+<title>About Me
+</title>
+</head>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Benne&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Big+Shoulders+Stencil+Display:wght@500&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Viaoda+Libre&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Kranky&display=swap" rel="stylesheet">
+
+<style type="text/css">
+
+.a1
+{ 
+font-family: cursive;
+font-size:180px;
+text-shadow:4px 4px 2px grey;
+
+}
+
+.a2
+{ 
+font-family: 'Benne', serif;
+font-size:60px;
+text-shadow:1px 1px 1px grey;
+}
+
+</style>
+
+
+
+<body>
+<center>
+<font class="a1"> About Me</font>
+</center>
+<hr size="+12" color="05007A"><br>
+<hr size="+12" color="05007A"><br><br>
+<br><br><br><br>
+<img src="131981332_687416125295516_8241565612227350279_n.jpg" width="100%" height="700px">
+<br><br><br><br><br><br>
+<hr size="+12" color="05007A"><br>
+<hr size="+12" color="05007A"><br>
+<hr size="+12" color="05007A"><br>
+<br><br><br><br><br><br>
+
+<font class="a1"> School life</font> <br><br>
+<hr size="+12" color="05007A"><br>
+
+<img src="" width="100%" height="700px"><br><br>
+
+<font class="a2"> I completed my school life from Atomic Energy Research Establishment School And College. I have passed almost 8 years of my life from here.   </font>
+<hr size="+12" color="05007A"><br><br>
+
+<font class="a1"> College life</font> <br><br>
+<hr size="+12" color="05007A"><br>
+
+<img src="" width="100%" height="700px"><br><br>
+
+<font class="a2"> After passing successfully SSC examination i got the chance into Notre Dame College, Dhaka. It was the best time i passed in the whole life.   </font>
+<hr size="+12" color="05007A"><br><br>
+
+
+
+</body>
+
+
+</html>
