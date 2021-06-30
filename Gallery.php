67d262efe1ea2c39a45c3975befd49c919fd6bfe
KhalidHossain3108 <86224418+KhalidHossain3108@users.noreply.github.com>
2021-06-30 22:26:53 +0600

    Update Gallery.php

--- Gallery.php
+++ Gallery.php
@@ -66,9 +66,9 @@ text-shadow:2px 2px 2px grey;
 <hr size="+12" color=#861D32><br><br>
 
 <br><br><br><br>
-<img src="" width="100%" height="1000px">
+<img src="inbound5140695674970281819.jpg" width="100%" height="1000px">
 <br><br>
-<font class="h2"> </font>
+<font class="h2">Find me if you can!! </font>
 <br><br><br><br><br><br>
 <hr size="+12" color=#861D32><br><br>
 <hr size="+12" color=#861D32><br><br>
