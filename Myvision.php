5bf28d69f0d886bbe96fa6eb37690b6d2745617d
KhalidHossain3108 <86224418+KhalidHossain3108@users.noreply.github.com>
2021-07-08 22:44:16 +0600

    Update Myvision.php

--- Myvision.php
+++ Myvision.php
@@ -1,8 +1,11 @@
 <html>
-<head>My vision
+<head>
+<title>My vision
+</title>
 <meta charset="UTF-8">
 </head>
-<body>
-<font size="+12"> This page is actually under construction,  don’t think i have no vision as the page remaining empty,  i will be available asap.😁</font>
+<body><br><br>
+<center><font size="+12"> This page is actually under construction,  don’t think i have no vision as the page remaining empty,  i will be available asap.😁,  </font>
+</center>
 </body>
 </html>
